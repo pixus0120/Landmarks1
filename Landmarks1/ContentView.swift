3cd4434a8011ae881f3643760e7f8eafaf8753a2
pixus0120 <ra.arcus@gmail.com>
2021-06-28 19:01:56 -0400

    Initial Commit

---
+++ Landmarks1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Landmarks1
+//
+//  Created by locussigilli on 6/28/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Landmarks1/Landmarks1App.swift
@@ -0,0 +1,17 @@
+//
+//  Landmarks1App.swift
+//  Landmarks1
+//
+//  Created by locussigilli on 6/28/21.
+//
+
+import SwiftUI
+
+@main
+struct Landmarks1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
