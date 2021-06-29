e15ecb256791c1fd7af8a102edf499b9bc541acc
pixus0120 <ra.arcus@gmail.com>
2021-06-29 16:29:15 -0400

    Building Lists and Navigation

---
+++ Landmarks1/CircleImage.swift
@@ -0,0 +1,23 @@
+//
+//  CircleImage.swift
+//  Landmarks1
+//
+//  Created by locussigilli on 6/28/21.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("turtlerock")
+            .clipShape(Circle())
+            .overlay(Circle().stroke(Color.white, lineWidth: 4))
+            .shadow(radius: 7)
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
