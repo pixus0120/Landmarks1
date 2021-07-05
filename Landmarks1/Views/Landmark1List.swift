d6fef43b8815b504dd293354b27b23d328495782
pixus0120 <ra.arcus@gmail.com>
2021-07-05 12:52:20 -0400

    Section 3 Add a Control to Toggle the State

--- Landmarks1/Views/Landmark1List.swift
+++ Landmarks1/Views/Landmark1List.swift
@@ -8,6 +8,14 @@
 import SwiftUI
 
 struct Landmark1List: View {
+    //Because you use state properties to hold information that’s specific to a view and its subviews, you always create state as private.
+    @State private var showFavoritesOnly = true
+    var filteredLandmarks: [Landmark1] {
+        landmarks.filter { landmark in
+            (!showFavoritesOnly || landmark.isFavorite)
+        }
+    }
+    
     var body: some View {
 //        List{
 //            Landmark1Row(landmark: landmarks[0])
@@ -16,7 +24,7 @@ struct Landmark1List: View {
         
         //Complete the dynamically-generated list by returning a LandmarkRow from the closure.This creates one LandmarkRow for each element in the landmarks array.
         NavigationView{
-            List(landmarks){ landmark in
+            List(filteredLandmarks){ landmark in
                 NavigationLink(destination: Landmark1Detail(landmark: landmark)){
                 Landmark1Row(landmark: landmark)
                 }
@@ -28,13 +36,16 @@ struct Landmark1List: View {
 
 struct Landmark1List_Previews: PreviewProvider {
     static var previews: some View {
-        //ForEach operates on collections the same way as the list, which means you can use it anywhere you can use a child view, such as in stacks, lists, groups, and more. When the elements of your data are simple value types — like the strings you’re using here — you can use:
-        //    \.self as key path to the identifier.
-        ForEach(["iPhone SE (2nd generation)", "iPhone XS Max"], id: \.self) { deviceName in
-            Landmark1List()
-                .previewDevice(PreviewDevice(rawValue: deviceName))
-                .previewDisplayName(deviceName)
-        }
+       Landmark1List()
+        
+        
+//        //ForEach operates on collections the same way as the list, which means you can use it anywhere you can use a child view, such as in stacks, lists, groups, and more. When the elements of your data are simple value types — like the strings you’re using here — you can use:
+//        //    \.self as key path to the identifier.
+//        ForEach(["iPhone SE (2nd generation)", "iPhone XS Max"], id: \.self) { deviceName in
+//            Landmark1List()
+//                .previewDevice(PreviewDevice(rawValue: deviceName))
+//                .previewDisplayName(deviceName)
+//        }
   
     }
 }
